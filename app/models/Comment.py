e93bd7b258dac89d2595bed24e6b347e2fba3f61
Yukitoshi <yukitoshi.imaizumizhou@gmail.com>
2024-06-25 17:16:54 +1000

    complete

--- app/models/Comment.py
+++ app/models/Comment.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from app.db import Base
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
-
 class Comment(Base):
   __tablename__ = 'comments'
   id = Column(Integer, primary_key=True)
@@ -11,5 +10,4 @@ class Comment(Base):
   post_id = Column(Integer, ForeignKey('posts.id'))
   created_at = Column(DateTime, default=datetime.now)
   updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
-
   user = relationship('User')
--- app/routes/home.py
+++ app/routes/home.py
@@ -1,22 +1,18 @@
 from flask import Blueprint, render_template, session, redirect
 from app.models import Post
 from app.db import get_db
-
 bp = Blueprint('home', __name__, url_prefix='/')
-
+#Copy
 @bp.route('/')
 def index():
   # get all posts
   db = get_db()
   posts = db.query(Post).order_by(Post.created_at.desc()).all()
-
   return render_template(
   'homepage.html',
   posts=posts,
-  loggedIn = session.get('loggedIn')
-  )
-
-
+  loggedIn=session.get('loggedIn')
+)
 @bp.route('/login')
 def login():
   # not logged in yet
@@ -24,7 +20,6 @@ def login():
     return render_template('login.html')
 
   return redirect('/dashboard')
-
 @bp.route('/post/<id>')
 def single(id):
   # get single post by id
