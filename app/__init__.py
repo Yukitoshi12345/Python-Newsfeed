2635f31ddca895035fd96160f95624265b7bd7c5
Yukitoshi <yukitoshi.imaizumizhou@gmail.com>
2024-06-25 16:07:41 +1000

    Set up User Logins and Sessions

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,7 @@
 from flask import Flask
-from app.routes import home, dashboard
+from app.routes import home, dashboard, api
 from app.db import init_db
 from app.utils import filters
-
 
 def create_app(test_config=None):
     # set up app config
@@ -14,6 +13,9 @@ def create_app(test_config=None):
     app.jinja_env.filters['format_url'] = filters.format_url
     app.jinja_env.filters['format_date'] = filters.format_date
     app.jinja_env.filters['format_plural'] = filters.format_plural
+
+    app.register_blueprint(api)
+    
 
     @app.route('/hello')
     def hello():
